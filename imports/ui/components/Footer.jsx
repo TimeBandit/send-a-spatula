ebdb9718e992ea3c9499f168d3b0184cadd17427
imran_nazir <imran.nazir@yahoo.co.uk>
2016-06-19 18:07:01 +0100

    add social buttons

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -5,7 +5,7 @@ import Credits from './Credits.jsx';
 const Footer = ({}) => (
     <footer>
 		<div className="row">
-			<div className="one-third column social">
+			<div className="column social">
 				<div className="facebook">
 					<a href="https://www.facebook.com/sendaspatula" target="_blank">
 						<i className="fa fa-2x fa-facebook-square" aria-hidden="true"></i>
@@ -21,21 +21,18 @@ const Footer = ({}) => (
 						<i className="fa fa-2x fa-envelope" aria-hidden="true"></i>
 					</a>
 				</div>					
-			</div>
-			<div className="one-third column shares">
+				<Credits />
+				<Disclaimer />
 				<div className="fb-like" 
 			        data-href="http://www.your-domain.com/your-page.html" 
 					data-layout="standard" 
 					data-action="like" 
-					data-show-faces="true"></div>
+					data-show-faces="true">
+				</div>
 		        <div className="tw-like">
 		        	<a href="https://twitter.com/share" className="twitter-share-button">Tweet</a>
 		        </div>
 			</div>
-			<div className="one-third column info">
-				<Credits />
-				<Disclaimer />
-			</div>				
 		</div>
 		<br/>
 		<div>
